2d0a68b9cd27eec2b8cbed5b7437f7924235566d
Michael Chavez <machavez2011@gmail.com>
2021-01-04 13:38:24 -0700

    Main crud components created for testing links and Router.

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>I am the edit-exercise component.</p>
+            </div>
+        )
+    }
+}
